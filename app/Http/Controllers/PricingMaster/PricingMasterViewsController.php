02b0b981121ccd9be6e38bbd4912fef2d63a1392
Shiv Kumar <software6@5core.com>
2025-10-14 01:58:22 +0530

    Update PricingMasterViewsController.php

--- app/Http/Controllers/PricingMaster/PricingMasterViewsController.php
+++ app/Http/Controllers/PricingMaster/PricingMasterViewsController.php
@@ -235,7 +235,7 @@ class PricingMasterViewsController extends Controller
 
         $ebayListingData = EbayListingStatus::whereIn('sku', $nonParentSkus)->get()->keyBy('sku');
         // $dobaData    = DobaMetric::whereIn('sku', $skus)->get()->keyBy('sku');
-        // $ebayData    = EbayMetric::whereIn('sku', $nonParentSkus)->get()->keyBy('sku');
+        $ebayData    = EbayMetric::whereIn('sku', $nonParentSkus)->get()->keyBy('sku');
         $temuListingData = TemuListingStatus::whereIn('sku', $nonParentSkus)->get()->keyBy('sku');
         $ebayTwoListingData = EbayTwoListingStatus::whereIn('sku', $nonParentSkus)->get()->keyBy('sku');
         $ebayThreeListingData = EbayThreeListingStatus::whereIn('sku', $nonParentSkus)->get()->keyBy('sku');
